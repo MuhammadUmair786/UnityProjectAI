4426c05d703ec472aac3d504fd64eaff81c60028
MuhammadIbrahim001 <60216701+MuhammadIbrahim001@users.noreply.github.com>
2020-06-11 11:51:25 +0500

    accept change brooo

--- UnityProject/Assets/Scripts/Enemy_FSM1.cs
+++ UnityProject/Assets/Scripts/Enemy_FSM1.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Enemy_FSM : MonoBehaviour
+public class Enemy_FSM1 : MonoBehaviour
 
 {
     private CheckMyVision checkMyVision;
